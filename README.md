8c38848d72d2ccf043c991a2d77ac261c556dee8
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2018-01-23 11:17:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Utility functions for dealing with MARC records [![NPM Version](https://img.shields.io/npm/v/@natlibfi/melinda-marc-record-utils.svg)](https://npmjs.org/package/@natlibfi/melinda-marc-record-utils) [![Build Status](https://travis-ci.org/NatLibFi/melinda-marc-record-utils.svg)](https://travis-ci.org/NatLibFi/melinda-marc-record-utils) [![Test Coverage](https://codeclimate.com/github/NatLibFi/melinda-marc-record-utils/badges/coverage.svg)](https://codeclimate.com/github/NatLibFi/melinda-marc-record-utils/coverage)
+
+Utility functions for dealing with MARC records
+
+## License and copyright
+
+Copyright (c) 2018 **University Of Helsinki (The National Library Of Finland)**
+
+This project's source code is licensed under the terms of **GNU Lesser General Public License Version 3** or any later version.
---
+++ test/index.spec.js
@@ -0,0 +1,36 @@
+/**
+*
+* @licstart  The following is the entire license notice for the JavaScript code in this file. 
+*
+* Utility functions for dealing with MARC records
+*
+* Copyright (C) 2018 University Of Helsinki (The National Library Of Finland)
+*
+* This file is part of melinda-marc-record-utils
+*
+* melinda-marc-record-utils is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* melinda-marc-record-utils is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* @licend  The above is the entire license notice
+* for the JavaScript code in this file.
+*
+*/
+
+/* eslint-disable no-undef, max-nested-callbacks, no-unused-expressions */
+
+'use strict';
+
+import {expect} from 'chai';
+import * as testContext from '../src/index';
+
+describe.skip('index');
